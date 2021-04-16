76a255bae2f8656cdad7a8183a5abf18d3a033a9
Holger Junker <holgerjunkerbsi@gmail.com>
2021-04-16 13:15:07 +0200

    fixed missing vba (sorry that was a real oopsie!)

--- ifiwasevilyoudbefucked.ps1
+++ ifiwasevilyoudbefucked.ps1
@@ -8,6 +8,10 @@ function Get-Permissions ($folder) {
           @{Label="Propagation Flags";Expression={$_.PropagationFlags}} | ft -auto
           }
 
+# TODOs
+# - make recursive crawling working
+# - file share for testing
+# - check which of all files (shares, partitions) are writeable, https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-childitem?view=powershell-7.1
 
 Write-Host "IfIWasEvilWouldYouBeFucked - ransomware risk check`r`n`r`n"
 
@@ -30,10 +34,25 @@ foreach ($extension in $extensions)
 
 foreach ($file in $files)
 {
+    #Write-Host $file;
+    #$tmppath = $file.Path + '\' + $file.Name;
+    #Write-Host "checking", $tmppath;
+    
+    # THIS CODE WOULD JUST PUT .xxx behind the current file extension (easily reversible)
+    #$tmpnewname = $file.Name + '.xxx';
+    #Rename-Item -Path $tmppath -NewName $tmpnewname;
+
     # CHECK IF FILE IS WRITEABLE --> COUNT TYPICAL FILETYPES OF INTEREST
     try {
         [System.IO.File]::OpenWrite($file).close();
         
+        $writable = Get-Item $file | select FullName, Name, Length, Extension
+        #[PSCustomObject] @{
+        #    FullName = $writable.FullName
+        #    Name = $writable.Name
+        #    Size = $writable.Length
+        #    Extension = $writable.Extension
+        #}
         foreach ($extension in $extensions)
         {
             if ($file.EndsWith($extension))
@@ -74,4 +93,36 @@ foreach ($extension in $extensions)
 }
 Write-Host "please note that I stopped myself before collecting too many files`r`n";
 
-Write-Host "`r`nBut wait... there is much more I could have done by now... perhaps take a look / lay my hand on some files on other disks, fileshares, ...`r`n"
+Write-Host "`r`nBut wait... there is much more I could have done by now... perhaps take a look / lay my hand on some files on other disks, fileshares, ... lets see...`r`n"
+
+#$disks = Get-Disk;
+#foreach ($disk in $disks)
+#{
+#    Write-Host $disk.Number, " (", $disk.Model, ", ", $disk.Size, ", read-only: ", $disk.IsReadOnly, ")";
+#}
+
+
+#Get-Partition | Format-List
+$partitions = Get-Partition;
+Write-Host "`r`npartitions of interest:"
+foreach ($partition in $partitions)
+{
+    if (($partition.DriveLetter -ge 'A' -and $partition.DriveLetter -le 'Z') -or ($partition.DriveLetter -ge 'a' -and $partition.DriveLetter -le 'z'))
+    {
+        Write-Host $partition.DriveLetter, ", size ", $partition.Size, ", type ", $partition.Type;
+    }
+}
+
+#Get-Permissions("C:\tdm\log.txt")
+
+Write-Host "`r`nshares of interest";
+$shares = Get-FileShare;
+foreach ($share in $shares)
+{
+    Write-Host $share.Name
+}
+
+#Get-Volume | Format-List
+
+#Get-Disk | Format-List
+#Get-ChildItem -Path $file -Recurse | ForEach-Object -Process {if (!$_.PSIsContainer) {$_.Name; $_.Length / 1024; " " }}
